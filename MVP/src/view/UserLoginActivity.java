806ba3d0d8efb8e1746bf0196fa9759100ee76eb
刘明明 <liumingming@16801.com>
2019-03-07 15:58:51 +0800

    一个简单的MVP

--- MVP/src/view/UserLoginActivity.java
+++ MVP/src/view/UserLoginActivity.java
@@ -3,15 +3,15 @@ package view;
 import presenter.UserLoginPresenter;
 import model.User;
 import android.R;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class UserLoginActivity extends ActionBarActivity implements IUserLoginView {
+public class UserLoginActivity extends Activity implements IUserLoginView {
 
 	private EditText mEtUsername,mEtPassword;
 	private Button mBtnLogin,mBtnClear;
